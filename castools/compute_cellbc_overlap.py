b2d1a8956a9999b9511609b0af2dc7fc58eb96d0
Avi R <trackavinash@gmail.com>
2021-09-17 11:21:36 -0500

    Add overlap computation code

---
+++ castools/compute_cellbc_overlap.py
@@ -0,0 +1,28 @@
+import gzip
+import sys
+
+"""
+ python3 compute_cellbc_overlap.py ../spike-in-090821-10xtranscriptome/SL5-1/outs/filtered_feature_bc_matrix/barcodes.tsv.gz SL4-1_cells_umis_tripbc.tsv
+"""
+
+tenx_cells = sys.argv[1]
+cas_cells_f = sys.argv[2]
+
+tenx_cellbcs = {}
+with gzip.open(tenx_cells, "rb") as fh:
+    for line in fh:
+        cellbc = line.decode().rstrip("-1\n")
+        tenx_cellbcs[cellbc]= 1
+
+
+cas_cellbcs = {}
+n_overlap = 0
+with open(cas_cells_f) as fh:
+    for line in fh:
+        cas_cell = line.rstrip("\n").split()[0]
+        if cas_cell not in cas_cellbcs:
+            cas_cellbcs[cas_cell] = 1
+            if cas_cell in tenx_cellbcs:
+                n_overlap += 1
+
+print(len(tenx_cellbcs), len(cas_cellbcs), n_overlap)
---
+++ castools/compute_tripbc_overlap.py
@@ -0,0 +1,26 @@
+import sys
+"""
+python3 compute_tripbc_overlap.py CAS_LP1_pool2_DNA_barcodes.txt SL4-1_cells_umis_tripbc.tsv
+"""
+
+bulk_trips = sys.argv[1]
+cas_trips_f = sys.argv[2]
+
+bulk_tripbcs = {}
+with open(bulk_trips) as fh:
+    for line in fh:
+        tripbc = line.rstrip("\n")
+        bulk_tripbcs[tripbc]= 1
+
+
+cas_tripbcs = {}
+n_overlap = 0
+with open(cas_trips_f) as fh:
+    for line in fh:
+        cas_trip = line.rstrip("\n").split()[2]
+        if cas_trip not in cas_tripbcs:
+            cas_tripbcs[cas_trip] = 1
+            if cas_trip in bulk_tripbcs:
+                n_overlap += 1
+
+print(len(bulk_tripbcs), len(cas_tripbcs), n_overlap)
