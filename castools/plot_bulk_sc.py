48895cd291ba3cb5d9720eba78ed7e5f0a2e18fd
Avi R <trackavinash@gmail.com>
2021-09-20 12:23:20 -0500

    Plot mean as well as mu

--- castools/plot_bulk_sc.py
+++ castools/plot_bulk_sc.py
@@ -5,6 +5,7 @@ import csv
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+from sklearn.linear_model import LinearRegression
 
 def parse_arguments():
     parser = argparse.ArgumentParser(description="Plot bulk means against sc means")
@@ -34,24 +35,31 @@ def read_sc(sc_f):
 def plot_bulk_sc(bulk_df, sc_df, prefix):
     merged = bulk_df.merge(sc_df, how = 'inner', on = 'tBC')
     print(merged.shape)
+    plt.figure(0)
     plt.scatter(merged['mu'], merged['exp'])
-    plt.savefig(prefix + "_bulk_sc_mean.png")
+    plt.xlabel("single-cell mu")
+    plt.ylabel("bulk exp")
+    plt.savefig(prefix + "_bulk_sc_mu.png")
     merged.to_csv(prefix + "_merged.tsv", sep = "\t")
-    from sklearn.linear_model import LinearRegression
     model = LinearRegression(fit_intercept = False)
-    #print(np.asarray(merged['mu']).reshape(-1, 1))
-    #print(np.asarray(merged['exp']))
     reg = model.fit(np.asarray(merged['mu']).reshape(-1, 1), np.asarray(merged['exp']))
-    print(np.asarray(merged['mu']).reshape(-1, 1))
-    print(np.asarray(merged['exp']))
-    print(np.sqrt(reg.score(np.asarray(merged['mu']).reshape(-1, 1), np.asarray(merged['exp']))))
+    print("mu")
     print(reg.score(np.asarray(merged['mu']).reshape(-1, 1), np.asarray(merged['exp'])))
+
+    plt.figure(1)
+    plt.xlabel("single-cell mean")
+    plt.ylabel("bulk exp")
+    plt.scatter(merged['mean'], merged['exp'])
+    plt.savefig(prefix + "_bulk_sc_mean.png")
+    model = LinearRegression(fit_intercept = False)
+    reg = model.fit(np.asarray(merged['mean']).reshape(-1, 1), np.asarray(merged['exp']))
+    print("mean")
+    print(reg.score(np.asarray(merged['mean']).reshape(-1, 1), np.asarray(merged['exp'])))
 
 def main():
     args = parse_arguments()
     bulk = read_bulk(args.bulk)
     sc = read_sc(args.sc)
-    print(bulk.head(), sc.head())
     plot_bulk_sc(bulk, sc, args.output)
 
 if __name__ == "__main__":
