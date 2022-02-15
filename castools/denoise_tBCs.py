d814badb154b9e5233246b8519f5944e9348f731
Avi R <trackavinash@gmail.com>
2022-02-14 20:40:47 -0600

    Update denoise - major

--- castools/denoise_tBCs.py
+++ castools/denoise_tBCs.py
@@ -6,20 +6,18 @@ import argparse
 
 
 
-write_hamming_lookup = False
 def calculate_hamming(hamming_lookup, rBC1, good_list):
     for rBC in good_list:
         pair1 = ":".join(sorted([rBC, rBC1]))
-        if pair1 in hamming_lookup:
-                hamming = hamming_lookup[pair1]
+        hamming = 100
+        if pair1 in hamming_lookup: # Save hamming distance computation by looking at precomputed hamming
+            hamming = hamming_lookup[pair1]
         else:
             hamming = hammingDist(rBC1, rBC)
             hamming_lookup[pair1] = hamming
-            write_hamming_lookup = True #some new pairs added
         if hamming < 2:
             return rBC
-        else:
-            return 0
+    return 0
 
 def hammingDist(str1, str2):
     '''
@@ -40,8 +38,9 @@ def filter_based_on_umi(quint_df, filter_BC_list, hamming_lookup, min_count = 2)
     Output: the unique quint that are supported by more than 1 read. 
     '''
     print("minimum read count is > ", min_count, file = sys.stderr)
-    print("Reading hamming lookup: ", hamming_lookup, file = sys.stderr)
+    write_hamming_lookup = True #Always update the lookup table
     if hamming_lookup is not None:
+        print("Reading hamming lookup: ", hamming_lookup, file = sys.stderr)
         with open(hamming_lookup, 'rb') as hl_fh:
             hamming_lookup = pickle.load(hl_fh)
     else:
@@ -49,9 +48,14 @@ def filter_based_on_umi(quint_df, filter_BC_list, hamming_lookup, min_count = 2)
         write_hamming_lookup = True
     cell_list = list(set(quint_df['cellBC'].values))
     pop_list = []
+    print("Going through cells in list", file = sys.stderr)
+    n = 0
+    seen_before = {} #Save some computation by keeping tripBC match
+    trash = {} # Don't waste time on these tBCs
     for cells in cell_list:
+        n += 1
+        print(f"cell {n} of {len(cell_list)}", file = sys.stderr)
         cell_df = quint_df[quint_df['cellBC'] == cells]
-        cell_list = list(set(quint_df['cellBC'].values))
         # Get the high count slice
         high_count_slice = cell_df[cell_df['count'] > min_count]
         # Filter the high count slice based on the filter_BC_list
@@ -59,14 +63,21 @@ def filter_based_on_umi(quint_df, filter_BC_list, hamming_lookup, min_count = 2)
         # Return a list for the high count slice
         high_count_tBC = set(high_count_slice['tBC'].values)
         for _, row in cell_df.iterrows():
-            if row['tBC'] in high_count_tBC:
+            if row['tBC'] in trash:
+                continue
+            elif row['tBC'] in high_count_tBC:
                 pop_list.append([row['cellBC'], row['umi'] , row['tBC'], row['count'] ])
+            elif row['tBC'] in seen_before:
+                pop_list.append([row['cellBC'], row['umi'] , seen_before[row['tBC']], row['count'] ])
             else:
                 possible_rBC = calculate_hamming(hamming_lookup, row['tBC'], high_count_tBC)
                 if possible_rBC != 0:
                     pop_list.append([row['cellBC'], row['umi'], possible_rBC , row['count']])
-                    #add this denoised tripBC to the high count set since it looks good
-                    high_count_tBC.add(row['cellBC'])
+                    #add this denoised tripBC to the seen before list
+                    seen_before[row['tBC']] = possible_rBC
+                else:
+                    trash[row['tBC']] = 1
+    print("Writing lookup and returning", file = sys.stderr)
     if write_hamming_lookup:
         print("Saving hamming lookup to hamming_lookup.pkl", file = sys.stderr)
         with open('hamming_lookup.pkl', 'wb') as fh:
@@ -86,7 +97,8 @@ def main():
     args= parser.parse_args()
     # Read in the Quad that contains the [cellBC, umi, tripBC, count], note even this is a tsv file
     # The delimiter is a space
-    prom_quint = pd.read_csv(args.quad, sep= ' ', names = ['cellBC', 'umi', 'tBC', 'count'])
+    prom_quint = pd.read_csv(args.quad, sep= '\t', names = ['cellBC', 'umi', 'tBC', 'count'])
+    print("Read quad: ", prom_quint.shape, file = sys.stderr)
     # Read in the bulk barcodes
     bulk_bcs_exp = pd.read_csv(args.bulkbcs, sep = '\t')
     # Process the bulk BCs to make it into a list
